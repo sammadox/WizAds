7cc0f5bd914a83fd033ea347ec04d05d8b783f17
Sammadox <81147507+sammadox@users.noreply.github.com>
2023-01-26 04:48:27 +0000

    Website Design

---
+++ To_keep_after_Installing_django/js/bar.js
@@ -0,0 +1,4 @@
+var svg = document.createElementNS("C:\Users\Sammad\Documents\GitHub\Pencila\To_keep_after_Installing_django\src\Pencil.svg", "svg");
+svg.setAttribute("x", "0");
+svg.setAttribute("y", "0");
+document.getElementById("svg-container").appendChild(svg);
